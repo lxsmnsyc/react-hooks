49cd9f32cab5aa33f32bb0b4ccfeb5f00903435a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-06-19 17:35:49 +0800

    Create useEvent.js

---
+++ src/hammer/useEvent.js
@@ -0,0 +1,26 @@
+import { useState, useEffect, MutableRefObject } from 'react';
+import Hammer from 'hammerjs';
+
+const register = (ref, event, options, setState) => {
+  if (ref instanceof HTMLElement) {
+    const instance = new Hammer(ref, options);
+    instance.on(event, setState);
+    return () => {
+      instance.off(event, setState);
+    };
+  }
+  return () => {};
+};
+
+export default (reference, event, options) => {
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    if (reference instanceof MutableRefObject) {
+      return register(reference.current, event, options, setState);
+    }
+    return register(reference, event, options, setState);
+  }, [event, options]);
+
+  return state;
+};
