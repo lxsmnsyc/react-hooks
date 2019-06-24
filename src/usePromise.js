58872fa4a57f56b3060e2055485a3e6bbf8f742c
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-06-24 17:30:04 +0800

    Update usePromise.js

--- src/usePromise.js
+++ src/usePromise.js
@@ -1,4 +1,31 @@
-import { useState } from 'react';
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2019 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2019
+ */
+import { useState, useEffect } from 'react';
 import useRefMounted from './useRefMounted';
 
 export default (promise) => {
@@ -6,21 +33,19 @@ export default (promise) => {
 
   const mounted = useRefMounted();
 
-  useCallback(() => {
-    setState({ loading: true });
+  useEffect(() => {
+    const evaluate = async () => {
+      try {
+        const result = await promise;
+        if (mounted.current) {
+          setState({ loading: true, value: result });
+        }
+      } catch (err) {
+        setState({ loading: false, error: err });
+      }
+    };
 
-    return promise.then(
-      (value) => {
-        if (mounted.current) {
-          setState({ value, loading: false});
-        }
-      },
-      (error) => {
-        if (mounted.current) {
-          setState({ error, loading: false});
-        }
-      },
-    );
+    evaluate();
   }, []);
 
   return state;
